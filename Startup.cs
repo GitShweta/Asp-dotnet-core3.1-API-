08f234832b4224bb2c5f4c255981ddf0ec759949
John Doe <johndoe@example.com>
2020-05-21 08:27:11 +0530

    another comment added

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,7 @@ namespace dotnet_rpg
 
         // This method gets called by the runtime. Use this method to add services to the container.
         //Cooment added
+        //another comment
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
